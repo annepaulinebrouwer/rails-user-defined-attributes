94b0b236541c3b8216467f29dd18db7f12585bda
Paula Brouwer <freelance.brouwer@gmail.com>
2023-09-29 11:57:21 +0200

    Write set_ and get_ methods for one class

--- app/models/battery.rb
+++ app/models/battery.rb
@@ -1,3 +1,11 @@
 class Battery < ApplicationRecord
   has_many :custom_attributes, as: :attribute_of, dependent: :destroy
+
+  def get_custom_attribute(key)
+    custom_attributes.where(key:)&.first&.value
+  end
+
+  def set_custom_attribute(key, value)
+    custom_attributes.create(key:, value:)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Clean database"
+Battery.destroy_all
+Customer.destroy_all
+
+puts "-------------"
+
+puts "Create a battery instance"
+b = Battery.create
+puts "Set custom attribute capacity of to 2500 mAh for this battery"
+b.custom_attributes.create(key: "capacity", value: "2500")
+
+puts "-------------"
+puts "Done"
